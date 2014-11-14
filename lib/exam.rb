bc66dea9d29b574190bc59f3d29b7c163d6aa1d2
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2014-11-14 01:19:06 +0000

    Modificando las directrices del exam.rb

--- lib/exam.rb
+++ lib/exam.rb
@@ -1,5 +1,11 @@
-require "exam/version"
+require "exam/version.rb"
+require "exam/simpleSelection.rb"
+require "exam/trueFalse.rb"
+require "exam/lista.rb"
+require "exam/pregunta.rb"
 
 module Exam
-  # Your code goes here...
+
+	#Your code goes here...
+
 end
